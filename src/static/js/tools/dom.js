65446bf636f9f28dfb1ff0e5fc78f9299b4c026d
wangqun28 <wangqun6@jd.com>
2022-08-01 08:24:04 +0800

    change

--- src/static/js/tools/dom.js
+++ src/static/js/tools/dom.js
@@ -1,3 +1,10 @@
+/*
+ * Created by king at 2022-7-28 12:37:12
+ * Copyright (c) 2022
+ */
+
+// 操作真实dom元素
+
 export const bubble = (dom, func) => {
     let cur = dom;
     while (cur && !func(cur)) {
@@ -16,6 +23,32 @@ export const isInCavans = target => {
         return false;
     });
     return result;
+};
+
+// 获取plot的dom元素
+const getPlotEl = target => {
+    let result;
+    bubble(target, dom => {
+        if (dom.dataset.id === 'lc-work-ground-component-canvas') {
+            // 只上溯到canvas画布
+            return true;
+        }
+        if (dom.dataset.lcParentWrapperUuid && dom.dataset.lcPlot) {
+            // 落在槽位上
+            result = dom;
+            return true;
+        }
+        return false;
+    });
+    return result;
+};
+
+export const getPlotElBoundingClientRect = target => {
+    const plot = getPlotEl(target);
+    if (plot) {
+        return plot.getBoundingClientRect();
+    }
+    return {};
 };
 
 // 必须是落在一个槽位上
@@ -38,4 +71,34 @@ export const getWidgetUUID = target => {
         return false;
     });
     return result;
+};
+
+export const element = (tag, props, parent) => {
+    let el = document.createElement(tag);
+    parent && parent.appendChild(el);
+    Object.keys(props).forEach(key => {
+        if (key === 'css') {
+            Object.keys(props[key]).forEach(cssKey => {
+                el.style.setProperty(cssKey, props[key][cssKey]);
+            });
+        } else if (key === 'children') {
+            props[key]?.forEach(child => {
+                el.appendChild(child);
+            });
+        } else if (key === 'className') {
+            el.className = props[key];
+        }
+    });
+    return {
+        el,
+        css(arg) {
+            Object.keys(arg).forEach(cssKey => {
+                el.style.setProperty(cssKey, arg[cssKey]);
+            });
+        },
+        remove() {
+            el.parentNode && parent.removeChild(el);
+            el = undefined;
+        }
+    };
 };
