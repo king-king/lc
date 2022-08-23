95b7757a434f57cd2cda0cf35b8be23f585b0bf3
wangqun28 <wangqun6@jd.com>
2022-08-23 15:45:54 +0800

    增加api菜单

--- src/static/js/pages/work/index.jsx
+++ src/static/js/pages/work/index.jsx
@@ -11,10 +11,18 @@ function Work() {
         <div>
             <div className='lc-work-header' />
             <div className='lc-work-ground'>
+                {/* 左侧导航 */}
+                <div className='lc-work-ground-left-menu'>
+                    <div className='lc-work-ground-icon-canvas' />
+                    <div className='lc-work-ground-icon-api' />
+                </div>
+                {/* 组件列表 */}
                 <div className='lc-work-ground-component-list' data-id='lc-work-ground-component-list'>
                     {widgetList.map(widget => <ComListItem key={widget.name} widget={{ ...widget }} />)}
                 </div>
+                {/* 中间内容绘制区域 */}
                 <Canvas />
+                {/* 组件配置 */}
                 <div className='lc-work-ground-component-config' data-id='lc-work-ground-component-config'>
                     <Edit />
                 </div>
