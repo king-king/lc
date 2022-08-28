b4187f582b95b5e4d4f0067b439376d0b06c0495
wangqun28 <wangqun6@jd.com>
2022-08-28 23:23:39 +0800

    更新动作页内容

--- src/static/js/pages/Work/Behavior/component/Add.jsx
+++ src/static/js/pages/Work/Behavior/component/Add.jsx
@@ -3,16 +3,30 @@
  * Copyright (c) 2022
  */
 
-import React from 'react';
-import { Typography } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Radio } from 'antd';
 
 const { Title } = Typography;
 
 function Add() {
+    const requestList = [{
+        label: '通用接口',
+        value: 'common_request'
+    }];
+    const behaviorList = [{
+        label: '通用行为',
+        value: 'common_behavior'
+    }];
+    const [value, setValue] = useState();
+    const onChange = item => {
+        setValue(item.target.value);
+    };
     return (
         <div className='lc-work-ground-api-add'>
-            <Title level={5}>请求</Title>
-            <Title level={5}>动作</Title>
+            <Title className='lc-work-ground-api-add-title' level={5}>请求</Title>
+            <Radio.Group className='lc-work-ground-api-add-radio' size='small' options={requestList} onChange={onChange} value={value} />
+            <Title className='lc-work-ground-api-add-title' level={5}>动作</Title>
+            <Radio.Group className='lc-work-ground-api-add-radio' size='small' options={behaviorList} onChange={onChange} value={value} />
         </div>
     );
 }
