38bc4b6e25a4c4e0292228b4854681fc9b86a3dc
wangqun28 <wangqun6@jd.com>
2022-09-02 18:23:47 +0800

    change

--- src/static/js/widget/Behavior/Edit/CommonApiReturnValue/index.jsx
+++ src/static/js/widget/Behavior/Edit/CommonApiReturnValue/index.jsx
@@ -4,6 +4,7 @@
  */
 
 // 专配置通用接口的返回值
+import { v4 as uuidv4 } from 'uuid';
 import {
     Input, Tooltip, Button, Popover, Select
 } from 'antd';
@@ -14,6 +15,7 @@ import './style';
 
 function CommonApiReturnValue() {
     const [list, setList] = useState([{
+        uuid: uuidv4(),
         key: 'key1',
         name: '指标1',
         desc: '指标描述',
@@ -21,14 +23,20 @@ function CommonApiReturnValue() {
     }]);
     const add = () => {
         setList([...list, {
-            key: `key${list.length}`, name: `指标${list.length}`, desc: '指标描述', formate: undefined
+            uuid: uuidv4(), key: `key${list.length}`, name: `指标${list.length}`, desc: '指标描述', formate: undefined
         }]);
     };
     const onDelete = index => {
         setList(list.filter((item, i) => i !== index));
     };
-    const onEditContentChange = () => {
+    const onEditContentChange = (uuid, propsName, value) => {
         const newList = JSON.parse(JSON.stringify(list));
+        newList.forEach(item => {
+            if (item.uuid === uuid) {
+                item[propsName] = value;
+            }
+        });
+        setList(newList);
     };
     const data = [
         { value: 'number', label: '整数' },
@@ -42,25 +50,25 @@ function CommonApiReturnValue() {
     const editContent = line => (
         <div>
             <FormItem label='名称'>
-                <Input size='small' defaultValue={line.name} onChange={e => onEditContentChange(line.key, 'name', e.target.value)} />
+                <Input size='small' defaultValue={line.name} onChange={e => onEditContentChange(line.uuid, 'name', e.target.value)} />
             </FormItem>
             <FormItem label='字段'>
-                <Input size='small' defaultValue={line.key} onChange={e => onEditContentChange(line.key, 'key', e.target.value)} />
+                <Input size='small' defaultValue={line.key} onChange={e => onEditContentChange(line.uuid, 'key', e.target.value)} />
             </FormItem>
             <FormItem label='类型'>
-                <Select size='small' style={{ width: '100%' }} defaultValue={line.formate} onChange={value => onEditContentChange(line.key, 'formate', value)}>
+                <Select size='small' style={{ width: '100%' }} defaultValue={line.formate} onChange={value => onEditContentChange(line.uuid, 'formate', value)}>
                     {data.map(item => <Select.Option key={item.value} value={item.value}>{item.label}</Select.Option>)}
                 </Select>
             </FormItem>
             <FormItem label='描述'>
-                <Input size='small' defaultValue={line.desc} onChange={e => onEditContentChange(line.key, 'desc', e.target.value)} />
+                <Input size='small' defaultValue={line.desc} onChange={e => onEditContentChange(line.uuid, 'desc', e.target.value)} />
             </FormItem>
         </div>
     );
     return (
         <FormItem label='返回值'>
             {list.map((item, index) => (
-                <Popover key={item.key} title='Title' trigger='click' content={editContent(item)}>
+                <Popover key={item.uuid} title='Title' trigger='click' content={editContent(item)}>
                     <Input
                         className='common-api-return-value'
                         value={`${item.name}（${item.key}）`}
